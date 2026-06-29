1cf6e5e405e82deff8ad7d2cf34e970f3d9a91e4
Yann Dos Santos <yann.dossantos@heig-vd.ch>
2026-06-29 16:52:04 +0200

    Added icons

--- components/ui/icons.h
+++ components/ui/icons.h
@@ -21,7 +21,22 @@ extern const uint8_t icon_settings[];
 #define ICON_STORAGE_H 32
 extern const uint8_t icon_storage[];
 
-/* volume.png: 32x32, 1 bpp, MSB-first. */
-#define ICON_VOLUME_W 32
-#define ICON_VOLUME_H 32
+/* volume.png: 16x16, 1 bpp, MSB-first. */
+#define ICON_VOLUME_W 16
+#define ICON_VOLUME_H 16
 extern const uint8_t icon_volume[];
+
+/* volume.png: 16x16, 1 bpp, MSB-first. */
+#define ICON_PLAY_W 16
+#define ICON_PLAY_H 16
+extern const uint8_t icon_play[];
+
+/* volume.png: 16x16, 1 bpp, MSB-first. */
+#define ICON_PREV_W 16
+#define ICON_PREV_H 16
+extern const uint8_t icon_prev[];
+
+/* volume.png: 16x16, 1 bpp, MSB-first. */
+#define ICON_NEXT_W 16
+#define ICON_NEXT_H 16
+extern const uint8_t icon_next[];
